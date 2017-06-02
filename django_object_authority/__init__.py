5cd4979e5598398c4ead49b27e25355ba9caa0a9
Tomeu Canyelles Escarrer <bcanyelles@apsl.net>
2017-06-02 15:32:15 +0200

    Version 0.0.3

--- django_object_authority/__init__.py
+++ django_object_authority/__init__.py
@@ -6,7 +6,7 @@ from .mixins import AuthorizationMixin
 from .options import BaseObjectAuthorization, BaseUserObjectAuthorization
 
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 
 default_app_config = 'django_object_authority.apps.DjangoObjectAuthorityConfig'
